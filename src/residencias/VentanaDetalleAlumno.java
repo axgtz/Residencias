7945783edab97b9d592cf9e37a011fd539b95d5b
Arturo Burela <uno@arturoburela.com>
2017-11-23 11:41:35 -0600

    Entradas

--- src/residencias/VentanaDetalleAlumno.java
+++ src/residencias/VentanaDetalleAlumno.java
@@ -5,9 +5,13 @@
  */
 package residencias;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,9 +21,12 @@ import javax.swing.JOptionPane;
 public class VentanaDetalleAlumno extends javax.swing.JFrame {
     
     Alumno alumno;
-    String[] horasEntrada;
+    ArrayList<String> horasEntrada;
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("ResidenciasPU");
     EntityManager em = emf.createEntityManager();
+    List<Habitacion> habitaciones;
+    ArrayList numerosHabitaciones;
+    String selectedHabitacion;
 
     /**
      * Creates new form VentanaDetalleAlumno
@@ -27,11 +34,16 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
      */
     public VentanaDetalleAlumno(Alumno a) {
         alumno = a;
-        horasEntrada = new String[alumno.getHorasentradaCollection().size()];
-        final int i = 0;
+        selectedHabitacion = null;
+        horasEntrada = new ArrayList();
         alumno.getHorasentradaCollection().forEach((temp) -> {
-            System.out.println(temp);
-            horasEntrada[i] = temp.getFecha().toString();
+            horasEntrada.add(temp.getFecha().toString());
+        });
+        TypedQuery<Habitacion> consultaHabitaciones = em.createNamedQuery("Habitacion.findAll", Habitacion.class);
+        habitaciones = consultaHabitaciones.getResultList();
+        numerosHabitaciones = new ArrayList();
+        habitaciones.forEach((temp) -> {
+            numerosHabitaciones.add(temp.getNumero());
         });
         initComponents();
     }
@@ -71,6 +83,8 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
         jList3 = new javax.swing.JList<>();
         jLabel7 = new javax.swing.JLabel();
         jButton6 = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(720, 480));
@@ -95,6 +109,11 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
         });
 
         jButton2.setText("Asignar Habitación");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Cargar");
 
@@ -108,7 +127,7 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
         });
 
         jList1.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = horasEntrada;
+            String[] strings = {"s","s"};
             public int getSize() { return strings.length; }
             public String getElementAt(int i) { return strings[i]; }
         });
@@ -121,7 +140,7 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
         });
         jScrollPane3.setViewportView(jList3);
 
-        jLabel7.setText("Habitacion 574");
+        jLabel7.setText(selectedHabitacion);
 
         jButton6.setText("Crear Reporte");
         jButton6.addActionListener(new java.awt.event.ActionListener() {
@@ -130,6 +149,10 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
             }
         });
 
+        jLabel8.setText(alumno.getMatricula() + ", " + alumno.getCarrera() + ", " + alumno.getEstatus());
+
+        jLabel9.setText("Disciplina deportiva: " + alumno.getDisciplinaDeportiva() + ", Estado de procedencia:  " + alumno.getEstadoProcedencia());
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -137,7 +160,6 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane3)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
@@ -149,22 +171,25 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel7)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 121, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton2))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton3))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel5)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton4))
+                    .addComponent(jScrollPane3)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
+                            .addComponent(jLabel8)
+                            .addComponent(jLabel9)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addGap(18, 18, 18)
+                                .addComponent(jButton3)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel5)
+                                .addGap(18, 18, 18)
+                                .addComponent(jButton4))
                             .addComponent(jLabel6)
                             .addComponent(jButton6))
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(0, 342, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -175,7 +200,11 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(jButton1)
                     .addComponent(jButton5))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel8)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel9)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(jButton2)
@@ -187,18 +216,16 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jButton3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton3)
                     .addComponent(jLabel5)
                     .addComponent(jButton4))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addComponent(jLabel6)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton6)
-                .addContainerGap(77, Short.MAX_VALUE))
+                .addGap(43, 43, 43))
         );
 
         pack();
@@ -240,6 +267,38 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
                 }
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        Object[] possibilities = new Object[habitaciones.size()];
+        possibilities = numerosHabitaciones.toArray(possibilities);  
+        Integer i = (Integer) JOptionPane.showOptionDialog(null,
+                null,  "ShowInputDialog",   
+                JOptionPane.PLAIN_MESSAGE, 1, null, possibilities, 0);
+        System.out.print(i);
+        AlumnohasHabitacion j = new AlumnohasHabitacion();
+        j.setAlumno(alumno);
+        j.setHabitacion(habitaciones.get(i));
+        j.setComentarios("ok");
+        em.getTransaction().begin();
+                try {
+                    em.detach(j);
+                    if (!em.contains(j)) {
+                        j = em.merge(j);
+                    }
+                    em.remove(j);
+                    em.getTransaction().commit();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    em.getTransaction().rollback();
+                    JOptionPane.showMessageDialog(null, "Error de conexión");
+                } finally {
+                    em.close();
+                    JOptionPane.showMessageDialog(null, "Habitación asignada con éxito");
+                    selectedHabitacion = String.valueOf(habitaciones.get(i).getNumero());
+                    jLabel7.setText(selectedHabitacion);
+                }
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -291,6 +350,8 @@ public class VentanaDetalleAlumno extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JList<String> jList1;
     private javax.swing.JList<String> jList3;
     private javax.swing.JScrollPane jScrollPane1;
